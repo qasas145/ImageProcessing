a570d40c2494cfe91babe8cafe0c50957017c68d
qasas145 <mohamed.sayed14527@yahoo.com>
2025-02-04 01:04:20 +0300

    Initial commit

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,37 @@
+﻿using ImageProcessing.Models;
+using ImageProcessing.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageProcessing.Controllers
+{
+    public class ImageController : Controller
+    {
+        private readonly IImageService _imageService;
+        public ImageController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+        public IActionResult Upload()
+        {
+            return View();
+        }
+        [HttpPost]
+        [RequestSizeLimit(100* 1024 * 1024)] // to prevent the user from uploading files which size is more than 100 MB
+        public async Task<IActionResult> Upload(IFormFile[] images)
+        {
+            var inputImages = images.Select(i => new ImageInputModel
+            {
+                FileName = i.FileName,
+                Type = i.ContentType,
+                Content = i.OpenReadStream(),
+
+            });
+            await _imageService.Process(inputImages);
+            return RedirectToAction(nameof(Done));
+        }
+        public IActionResult Done()
+        {
+            return View();
+        }
+    }
+}
---
+++ Services/IImageService.cs
@@ -0,0 +1,11 @@
+﻿using ImageProcessing.Models;
+using SixLabors.ImageSharp;
+
+namespace ImageProcessing.Services
+{
+    public interface IImageService
+    {
+        Task SaveImageAsync(Image image, string name, int resizeWidth);
+        Task Process(IEnumerable<ImageInputModel> images);
+    }
+}
