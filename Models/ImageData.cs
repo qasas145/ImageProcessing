721d34d21bf91ec59c51b5e55558f43a68546cd2
qasas145 <mohamed.sayed14527@yahoo.com>
2025-02-04 18:44:59 +0300

    Image database model

---
+++ Models/ImageData.cs
@@ -0,0 +1,14 @@
+﻿namespace ImageProcessing.Models
+{
+    public class ImageData
+    {
+        public ImageData() => Id = Guid.NewGuid();
+        public Guid Id { get; set; }
+        public string OriginalFileName { get;set; }
+        public string OriginalType { get; set; }
+        public byte[] OriginalContent { get;set; }
+        public byte[] ThumbnailContent { get; set; }
+        public byte[] FullScreenContent { get;set; }
+
+    }
+}
