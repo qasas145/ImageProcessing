2cd54c3f0dad824a3a31d43c05868ec968ecd23d
qasas145 <mohamed.sayed14527@yahoo.com>
2025-02-04 18:42:08 +0300

    DbContext file

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ImageProcessing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ImageProcessing.Data
+{
+    public class ApplicationDbContext  : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<ImageData> ImageData { get; set; }
+    }
+}
