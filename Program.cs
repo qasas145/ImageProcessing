46d872da64fe058f83763971995bd264ee0f670e
qasas145 <mohamed.sayed14527@yahoo.com>
2025-02-04 18:42:57 +0300

    caching website static files

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
+using ImageProcessing.Data;
 using ImageProcessing.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IImageService, ImageService>();
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options => 
+        options
+        .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +28,22 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapStaticAssets();
+// this will make caching for the files like 
+// site.css and site.js and so on .. not the files we return in the response 
+app.UseStaticFiles(new StaticFileOptions
+{
+    OnPrepareResponse = prepration =>
+    {
+        Console.WriteLine(prepration.File.Name);
+        var headers = prepration.Context.Response.GetTypedHeaders();
+        headers.CacheControl = new CacheControlHeaderValue
+        {
+            Public = true,
+            MaxAge = TimeSpan.FromMinutes(5)
+        };
+        headers.Expires = new DateTimeOffset(DateTime.UtcNow.AddMinutes(5));
+    }
+});
 
 app.MapControllerRoute(
     name: "default",
